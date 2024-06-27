7ca78a0cd3b8c72b36a42730e00593047b70bedc
Your Name <fernando_contas@yahoo.com.br>
2024-06-27 13:18:34 -0300

    Atualizações importantes

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+# Função para redimensionar uma imagem mantendo a proporção
+def redimensionar_imagem(img, largura_desejada):
+    proporcao = largura_desejada / img.shape[1]
+    altura_desejada = int(img.shape[0] * proporcao)
+    return cv2.resize(img, (largura_desejada, altura_desejada))
+
 def lineDashBoardColor(laneImage, infoLines):
     if infoLines == "left and right":
         cv2.line(laneImage, pt1=(100,80), pt2=(100,160), color=(0,255,0), thickness=10)
@@ -51,7 +57,7 @@ def makeCoordinates(image, lineParameters):
     #pegando a base da imagem
     y1 = image.shape[0]
     #pegando o "meio" da imagem
-    y2 = int(y1*(3/5))
+    y2 = int(y1*(4/5))
     x1 = int((y1 - intercept)/slope)
     x2 = int((y2 - intercept)/slope)
     return np.array([x1,y1,x2,y2])
@@ -72,9 +78,16 @@ def averageSlopeIntercept(image, lines):
         slope = parameters[0]
         intercept = parameters[1]
         # se o coeficiente angular da reta/linha for negativo, então é uma linha da esquerda (lembre-se que neste plano carteziano o eixo y tem seu valor minimo no extremo e seu valor maximo proximo a onde o eixo x tem valor 0)
-        if slope < 0:
+
+        if slope < -0.5:
+            #print("--------------------------")
+            #print(f"x1: {x1}, y1: {y1} x2: {x2} y2: {y2}")
+            #print("{:.2f}".format(slope))
             leftFit.append((slope, intercept))
-        else:
+        elif slope > 0.5:
+            #print("--------------------------")
+            #print(f"x1: {x1}, y1: {y1} x2: {x2} y2: {y2}")
+            #print("{:.2f}".format(slope))
             rightFit.append((slope, intercept))
 
 
@@ -101,13 +114,24 @@ def averageSlopeIntercept(image, lines):
     else:
         return ("nothing",np.array([]))
 
+def cannySatura(image):
+    rgb2hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    rgb2hls[:, :, 2] = 255
+    hls2rgb = cv2.cvtColor(rgb2hls, cv2.COLOR_HLS2RGB)
+    gray = cv2.cvtColor(hls2rgb, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0) #reduzir ruido
+    minThreshold = 50 #todo brilho abaixo do minThreshold será desconsiderado como borda
+    maxThreshold = 150 #todo brilho acima do maxThreshold será concerteza considerado como borda
+    #o que acontece entre os valores minThreshold e maxThreshold é mais complicado 
+    canny = cv2.Canny(blur, minThreshold, maxThreshold)
+    kernel = np.zeros((3,3), np.uint8)
+    erode = cv2.erode(canny, kernel, iterations=1)
+    return erode
+
 def canny(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    lowThreshold = 50
-    highThreshold = 150
-    canny = cv2.Canny(blur, lowThreshold, highThreshold)
-    return canny
+    h,l,s = cv2.split(cv2.cvtColor(image,cv2.COLOR_BGR2HLS))
+    cannyImage = cv2.Canny(l,100, 150)
+    return cannyImage
 
 def displayLines(image, lines):
     # uma imagem preta, pois é uma matriz de zeros, da mesma resolução que a imagem
@@ -118,8 +142,31 @@ def displayLines(image, lines):
             # se a linha não é nula e possui as quatro coordenadas necessarias para traçar uma reta
             if line is not None and len(line) == 4:
                 x1,y1,x2,y2 = line
-                #desenhando linhas sobre a imagem preta. As linhas utilizam 2 pontos para serem desenhadas
-                cv2.line(line_image,(x1,y1),(x2,y2), (0,0,255), thickness=10)
+                try:
+                    #desenhando linhas sobre a imagem preta. As linhas utilizam 2 pontos para serem desenhadas
+                    cv2.line(line_image,(x1,y1),(x2,y2), (0,0,255), thickness=10)
+                except:
+                    print("Gabriel")
+    return line_image
+
+def displayAllLines(image, lines):
+    # uma imagem preta, pois é uma matriz de zeros, da mesma resolução que a imagem
+    line_image = np.zeros_like(image)
+    #verificamos se a matriz de linhas possui alguma linha
+    if lines is not None and len(lines) > 0:
+        for line in lines:
+            allLines = line[0]
+            # se a linha não é nula e possui as quatro coordenadas necessarias para traçar uma reta
+            if line is not None and len(allLines) == 4:
+                x1,y1,x2,y2 = allLines
+                parameters = np.polyfit((x1,x2),(y1,y2), 1)
+                slope = parameters[0]
+                if abs(slope) > 0.4: #ignora linhas com coeficiente angular no intervalo ]0.5, -0.5[
+                    try:
+                        #desenhando linhas sobre a imagem preta. As linhas utilizam 2 pontos para serem desenhadas
+                        cv2.line(line_image,(x1,y1),(x2,y2), (0,0,255), thickness=10)
+                    except:
+                        print("Gabriel")
     return line_image
 
 def plotCannyGraphic(canny):
@@ -129,10 +176,10 @@ def plotCannyGraphic(canny):
 
 def regionOfInterest(image):
     alturaDaImagem = image.shape[0] #o parametro 0 do metodo shape, retorna o numero de linhas.
-    verticeBaseEsquerda = (0,alturaDaImagem) #antes era 178
-    verticeBaseDireita = (1920,alturaDaImagem)
-    verticeTopoEsquerda = (557,716)
-    verticeTopoDireita = (1370,716)
+    verticeBaseEsquerda = (570,alturaDaImagem-200) #antes era 178
+    verticeBaseDireita = (1350,alturaDaImagem-200)
+    verticeTopoEsquerda = (770,750)
+    verticeTopoDireita = (1150,750)
 
     #definição dos vertices do trapezio. É uma lista porque fillPoly só aceita listas de poligonos.
     vertices = np.array([
@@ -148,7 +195,7 @@ def regionOfInterest(image):
     return masked_image
 
 def main():
-    videoPath = "./videos/ultrapassagens.mp4"
+    videoPath = "./videos/asfalto_imply.mp4"
     capture = cv2.VideoCapture(videoPath)
     linhaIdentificada = 0
     linhaNaoIdentificada = 0
@@ -160,8 +207,8 @@ def main():
         if ret is False:
             break
 
-        laneImage = np.copy(image)
-        cannyImage = canny(laneImage)
+        baseImage = np.copy(image)
+        cannyImage = canny(baseImage)
         croppedImage = regionOfInterest(cannyImage)
 
         #detectando linhas
@@ -172,35 +219,42 @@ def main():
         if lines is not None:
             linhaIdentificada += 1
 
-            infoLines, averagedLines = averageSlopeIntercept(laneImage, lines)
+            infoLines, averagedLines = averageSlopeIntercept(baseImage, lines)
             if verificaMudancaDeFaixa(infoLines, averagedLines):
                 # escrevendo texto na imagem
-                cv2.putText(laneImage, "Mudanca de faixa", (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
-                
+                cv2.putText(baseImage, "Mudanca de faixa", (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)    
             else:
-                cv2.putText(laneImage, "Dentro das linhas", (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+                cv2.putText(baseImage, "Dentro das linhas", (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
 
             # pinta linhas no dashboard (canto superior esquerdo)
-            lineDashBoardColor(laneImage, infoLines)
-            
+            lineDashBoardColor(baseImage, infoLines)
 
             if averagedLines is not None and len(averagedLines) > 0:
-                lineImage = displayLines(laneImage, averagedLines)
+                #lineImage = displayLines(laneImage, averagedLines)
+                lineImage = displayLines(baseImage, averagedLines)
                 #Vamos combinar a imagem que mostra as linhas com a imagem real. Para isso, somamos a imagem real com a imagem preta que possui linhas azuis, deste modo os pixels pretos, que valem zero, não irão modificar a imagem, porém, os pixels azuis, que valem 255, irão modificar a imagem.
-                comboImage = cv2.addWeighted(laneImage, 1, lineImage, 1, 1)
+                comboImage = cv2.addWeighted(baseImage, 1, lineImage, 1, 1)       
             else:
-                comboImage = laneImage
+                comboImage = baseImage
 
         #se linhas não foram identificadas
         else:
-            cv2.putText(laneImage, "Indefinido", (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,165,255), 2)
+            cv2.putText(baseImage, "Indefinido", (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,165,255), 2)
             #pinta linhas no dashboard
-            lineDashBoardColor(laneImage, infoLines)
-            comboImage = laneImage
+            lineDashBoardColor(baseImage, "")
+            comboImage = baseImage
             linhaNaoIdentificada += 1
             
+        # Definir a largura desejada para a exibição
+        largura_janela = 1200
+       
+        #prints das diferentes imagens
+        #cv2.imshow("Normal", laneImage)
+        #cv2.imshow("Cropped RGB", redimensionar_imagem(regionOfInterest(laneImage), 1200))
+        cv2.imshow("comboImage", redimensionar_imagem(comboImage, largura_janela))
+        #cv2.imshow("croppedImage", redimensionar_imagem(croppedImage, largura_janela))
+        #cv2.imshow("grayImage", redimensionar_imagem(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY), largura_janela))
 
-        cv2.imshow("Saida", comboImage)
         #interrompe loop se a tecla esc for pressionada
         if cv2.waitKey(40) == 27:
             break
