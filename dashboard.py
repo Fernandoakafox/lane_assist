16b81951dcf77d4359abd49ffe9b29a13d3afaf9
Your Name <fernando_contas@yahoo.com.br>
2024-07-03 10:05:28 -0300

    refactor(dashboard) lineDashBoardColor was refactored to improve code readability.

--- dashboard.py
+++ dashboard.py
@@ -1,35 +1,34 @@
 import cv2
+from cores_bgr import CoresBGR
 
 class Dashboard:  
     def show(mensagem,frame):
-        cor = (0,0,0)
+        cor = CoresBGR.BRANCO
         match(mensagem):
             case "Mudanca de faixa":
-                cor = (0,0,255)   #vermelho
+                cor = CoresBGR.VERMELHO
             case "Dentro das faixas":
-                cor = (0,255,0)   #verde
+                cor = CoresBGR.VERDE
             case "Indefinido":
-                cor = (0,165,255) #laranja
+                cor = CoresBGR.LARANJA
             case _:
-                cor = (0,0,0)
-
-            
+                cor = CoresBGR.BRANCO
+  
         cv2.putText(frame, mensagem, (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, cor, 2)
 
-    #TODO modularizar/encurtar/endireitar
-    def lineDashBoardColor(frame, infoLines):
-        if infoLines == "left and right":
-            cv2.line(frame, pt1=(100,80), pt2=(100,160), color=(0,255,0), thickness=10)
-            cv2.line(frame, pt1=(120,80), pt2=(120,160), color=(0,255,0), thickness=10)
-                
-        elif infoLines == "left":
-            #desenhar uma reta na imagem
-            cv2.line(frame, pt1=(100,80), pt2=(100,160), color=(0,255,0), thickness=10)
-            cv2.line(frame, pt1=(120,80), pt2=(120,160), color=(128,128,128), thickness=10)
-        elif infoLines == "right":
-            #desenhar uma reta na imagem
-            cv2.line(frame, pt1=(100,80), pt2=(100,160), color=(128,128,128), thickness=10)
-            cv2.line(frame, pt1=(120,80), pt2=(120,160), color=(0,255,0), thickness=10)
-        else:
-            cv2.line(frame, pt1=(100,80), pt2=(100,160), color=(128,128,128), thickness=10)
-            cv2.line(frame, pt1=(120,80), pt2=(120,160), color=(128,128,128), thickness=10)
+    def lineDashBoardColor(frame, info_lines):
+        match(info_lines):
+            case "left and right":
+                right_color = left_color = CoresBGR.VERDE
+            case "left":
+                left_color = CoresBGR.VERDE
+                right_color = CoresBGR.CINZA
+            case "right":
+                left_color = CoresBGR.CINZA
+                right_color = CoresBGR.VERDE
+            case _:
+                right_color = left_color = CoresBGR.CINZA
+            
+        #desenhar uma reta na imagem
+        cv2.line(frame, pt1=(100,80), pt2=(100,160), color=left_color, thickness=10)
+        cv2.line(frame, pt1=(120,80), pt2=(120,160), color=right_color, thickness=10)
