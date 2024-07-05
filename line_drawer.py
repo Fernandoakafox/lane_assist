b534d084f252df47fc335a69622e1eb695e634a4
Your Name <fernando_contas@yahoo.com.br>
2024-07-05 15:25:56 -0300

    feat(line_drawer): Added a new function called draw_all_lines.

--- line_drawer.py
+++ line_drawer.py
@@ -13,6 +13,14 @@ class LineDrawer:
         else:
             return frame
         
+    def draw_all_lines(lines, frame):
+        if lines is not None and len(lines) > 0:
+            lineImage = LineDrawer._display_lines(frame, lines)
+            #Vamos combinar a imagem que mostra as linhas com a imagem real. Para isso, somamos a imagem real com a imagem preta que possui linhas azuis, deste modo os pixels pretos, que valem zero, não irão modificar a imagem, porém, os pixels azuis, que valem 255, irão modificar a imagem.
+            return cv2.addWeighted(frame, 1, lineImage, 1, 1)    
+        else:
+            return frame
+
     @staticmethod
     def _display_lines(frame, lines):
         # uma imagem preta, pois é uma matriz de zeros, da mesma resolução que a imagem
@@ -21,11 +29,15 @@ class LineDrawer:
         if lines is not None and len(lines) > 0:
             for line in lines:
                 # se a linha não é nula e possui as quatro coordenadas necessarias para traçar uma reta
-                if line is not None and len(line) == 4:
-                    x1,y1,x2,y2 = line
-                    try:
-                        #desenhando linhas sobre a imagem preta. As linhas utilizam 2 pontos para serem desenhadas
-                        cv2.line(line_image,(x1,y1),(x2,y2), (0,0,255), thickness=10)
-                    except:
-                        print("Erro display_lines")
+                if line is not None and len(line[0]) == 4:
+                    x1,y1,x2,y2 = line[0]
+                    parameters = np.polyfit((x1,x2),(y1,y2), 1)
+                    slope = parameters[0]
+                    intercept = parameters[1]
+                    if slope < -0.5 or slope > 0.5:
+                        try:
+                            #desenhando linhas sobre a imagem preta. As linhas utilizam 2 pontos para serem desenhadas
+                            cv2.line(line_image,(x1,y1),(x2,y2), (0,0,255), thickness=10)
+                        except:
+                            print("Erro display_lines")
         return line_image
